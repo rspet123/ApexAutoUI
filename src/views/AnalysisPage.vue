f53bfbd4d96e670ae47317dc7f1b12eeadaafe60
Robert Spencer <rspenc19@uncc.edu>
2024-11-26 01:23:42 -0500

    Add Slider to control # of screenshots to process

--- src/views/AnalysisPage.vue
+++ src/views/AnalysisPage.vue
@@ -4,9 +4,15 @@
     <v-container>
         <v-row>
             <v-col cols="12">
-            <v-btn @click="startAnalysis">Start Analysis</v-btn>
-            <v-btn @click="stopAnalysis">Stop Analysis</v-btn>
-            <v-btn @click="resetAnalysis">Reset Analysis</v-btn>
+                <v-btn @click="startAnalysis">Start Analysis</v-btn>
+                <v-btn @click="stopAnalysis" disabled>Stop Analysis*</v-btn> 
+                <v-btn @click="resetAnalysis">Reset Analysis</v-btn> 
+            </v-col>
+            <v-col>
+                <h3>
+                    Number of Screenshots to Analyze
+                </h3>
+                <v-slider v-model="numToAnalyze" :max="screenshotCount" thumb-label="always" step="1"></v-slider>
             </v-col>
         </v-row>
     </v-container>
@@ -19,11 +25,14 @@ import api from "../api";
 export default {
   name: "AnalysisPage",
     data() {
-        return {};
+        return {
+            screenshotCount: 0,
+            numToAnalyze: 0,
+        };
     },
     methods: {
         startAnalysis() {
-            api.post('/analyze')
+            api.post('/analyze', { num_to_analyze: this.numToAnalyze })
                 .then(response => {
                     console.log('Analysis started:', response);
                 })
@@ -48,7 +57,17 @@ export default {
                 .catch(error => {
                     console.error('Error resetting analysis:', error);
                 });
-        }
+        },
+        getScreenshotCount() {
+            api.get('/analysis_status')
+        .then(response => {
+          this.screenshotCount = response.data.total_unprocessed;
+          print(response.data.total_unprocessed);
+        })
+        .catch(error => {
+          console.error('Error fetching analysis status:', error);
+        });
+        },
 
     }
 };
